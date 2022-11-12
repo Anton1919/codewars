ec3c1704bf40f6f3cf9f4531b1df4f6dc770efa5
Anton1919 <bf3kenzo@mail.ru>
2022-11-12 17:32:43 +0300

    task

--- scripts.js
+++ scripts.js
@@ -4,10 +4,19 @@
 
 // console.log(buildString('Cheese','Milk','Chocolate'));
 
-function isPalindrome(x) {
-  return x.split('').reverse().join('').toLowerCase() === x.toLowerCase()
-    ? true
-    : false;
+// function isPalindrome(x) {
+//   return x.split('').reverse().join('').toLowerCase() === x.toLowerCase()
+//     ? true
+//     : false;
+// }
+
+// console.log(isPalindrome('Abba'));
+
+function getAverage(marks) {
+  const sum = marks.reduce((a, b) => a + b);
+  const sum2 = Math.floor(sum / marks.length);
+
+  return sum2;
 }
 
-console.log(isPalindrome('Abba'));
+console.log(getAverage([2, 2, 2, 2]));
