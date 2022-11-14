4efbea6b6ca8ab559929e5b824d91864de4511a4
Anton1919 <bf3kenzo@mail.ru>
2022-11-14 14:02:57 +0300

    8ku

--- scripts.js
+++ scripts.js
@@ -12,11 +12,21 @@
 
 // console.log(isPalindrome('Abba'));
 
-function getAverage(marks) {
-  const sum = marks.reduce((a, b) => a + b);
-  const sum2 = Math.floor(sum / marks.length);
+// function getAverage(marks) {
+//   const sum = marks.reduce((a, b) => a + b);
+//   const sum2 = Math.floor(sum / marks.length);
 
-  return sum2;
+//   return sum2;
+// }
+
+// console.log(getAverage([2, 2, 2, 2]));
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === 'Zach') {
+    return 18;
+  } else {
+    return 0;
+  }
 }
 
-console.log(getAverage([2, 2, 2, 2]));
+console.log(howManyLightsabersDoYouOwn('Zach'));
