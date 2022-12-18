4b09a119ea264deab3d18f1d1c15817191e0b6ce
Anton1919 <bf3kenzo@mail.ru>
2022-12-18 11:58:24 +0300

    7ku

--- scripts.js
+++ scripts.js
@@ -188,21 +188,22 @@ console.log(orderedCount("abracadabra"))
 // }
 
 
-function DNAStrand(dna){
 
-    let result = []
-    for (let i = 0; i < dna.length; i++) {
-        dna[i] === "A" && result.push("T")
-        dna[i] === "T" && result.push("A")
-        dna[i] === "G" && result.push("C")
-        dna[i] === "C" && result.push("G")
-
-        console.log(dna[i])
-    }
-    return result.join("")
-}
-
-console.log(DNAStrand("ATTGC"))
+// function DNAStrand(dna){
+//
+//     let result = []
+//     for (let i = 0; i < dna.length; i++) {
+//         dna[i] === "A" && result.push("T")
+//         dna[i] === "T" && result.push("A")
+//         dna[i] === "G" && result.push("C")
+//         dna[i] === "C" && result.push("G")
+//
+//         console.log(dna[i])
+//     }
+//     return result.join("")
+// }
+//
+// console.log(DNAStrand("ATTGC"))
 //"TAACG"
 // function isInStrictMode() {
 //  return  !this
