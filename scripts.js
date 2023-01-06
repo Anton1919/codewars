e5ba933df48d1608735b7ed7709fb2dfd5875806
Anton1919 <bf3kenzo@mail.ru>
2023-01-06 13:49:19 +0300

    7

--- scripts.js
+++ scripts.js
@@ -313,5 +313,13 @@ console.log(orderedCount("abracadabra"))
 // //  console.log(complete("AaaaA")) // "AaaaAaaaA"
 // console.log(complete("xyz")) // "xyzyx"
 
+const sumTo = (num) => {
+    let result = 0
 
+    for (let i = 1; i <= num; i++) {
+        result = result + i
+    }
+    return result
+}
 
+console.log(sumTo(3))
