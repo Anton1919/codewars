6da8d9188bbed5b560b761706aebf2a60bdc4a74
Anton1919 <bf3kenzo@mail.ru>
2023-01-29 14:01:40 +0300

    7ky

--- scripts.js
+++ scripts.js
@@ -446,11 +446,21 @@ console.log(barTriang([4, 6], [12, 4], [10, 10]))*/ // [8.6667, 6.6667]
 // console.log(arr2)
 
 
-function sumOfIntegersInString(s) {
+// function sumOfIntegersInString(s) {
+//
+// return console.log(s
+//     .split(" ").map(el => Number(el)).reduce((a, b) => a + b))
+//
+// }
+//
+// console.log(sumOfIntegersInString("from 1929 to 1939.")) //18
 
-return console.log(s
-    .split(" ").map(el => Number(el)).reduce((a, b) => a + b))
-
+function makeMove(sticks) {
+    if(sticks % 4 == 0) {
+        return Math.random() * 3 +1
+    }
+    return sticks % 4
 }
 
-console.log(sumOfIntegersInString("from 1929 to 1939.")) //18
+console.log(makeMove(21))
+
